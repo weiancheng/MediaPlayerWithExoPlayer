109820594e3373fd9c63e6ad117a1e145a05605c
Weian Cheng <weian_cheng@htc.com>
2017-11-28 09:52:58 +0800

    first commit, add files.

---
+++ app/src/main/java/weian/cheng/mediaplayerwitexoplayer/MainActivity.kt
@@ -0,0 +1,11 @@
+package weian.cheng.mediaplayerwitexoplayer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity:AppCompatActivity() {
+    protected override fun onCreate(savedInstanceState:Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
