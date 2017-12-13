5da8aa9278ae81d49867e8baeb76731fe781a838
Weian <d92434101@gmail.com>
2017-12-13 22:51:19 +0800

    add comments for APIs

--- mediaplayerwithexoplayer2/src/main/java/weian/cheng/mediaplayerwithexoplayer/IMusicPlayer.kt
+++ mediaplayerwithexoplayer2/src/main/java/weian/cheng/mediaplayerwithexoplayer/IMusicPlayer.kt
@@ -2,15 +2,54 @@ package weian.cheng.mediaplayerwithexoplayer
 
 /**
  * Created by weian on 2017/12/10.
+ *
+ * The interface for the music player.
  */
 
 interface IMusicPlayer {
+    /**
+     * Start playing a music.
+     * This function will play the music which is specified with an URI.
+     */
     fun play(uri: String)
+
+    /**
+     * This function is also about play the music, but when the music is playing,
+     * executing this function will pause the music.
+     * If the music is pausing, executing this function will resume the music.
+     */
     fun play()
+
+    /**
+     * stop playing the music.
+     */
     fun stop()
+
+    /**
+     * pause a music. If no music is played, nothing to do.
+     */
     fun pause()
+
+    /**
+     * resume the playing of the music.
+     */
     fun resume()
+
+    /**
+     * set the repeat mode: normal play, repeat one music, repeat the whole playlist
+     */
     fun setRepeat(isRepeat: Boolean)
+
+    /**
+     * seek the play time when the music is playing
+     */
     fun seekTo(sec: Int)
+
+    /**
+     * The function is used to get the current state of the music player.
+     * Standby: the music player is waiting for the music.
+     * Play: the music player is playing.
+     * Pause: the music player is pausing.
+     */
     fun getPlayerState(): MusicPlayerState
 }
